2271d8054343c488d4c98d2627845bdc09c77dd5
Kieran Northime <kieran@blackeyetech.com>
2020-09-17 17:19:33 +0100

    Changing console logging to help with memory leak

--- src/cn-logger-console.ts
+++ src/cn-logger-console.ts
@@ -1,4 +1,5 @@
 import CNLogger from "./cn-logger";
+import util from "util";
 
 class CNLoggerConsole extends CNLogger {
   constructor(name: string) {
@@ -8,52 +9,74 @@ class CNLoggerConsole extends CNLogger {
   fatal(...args: any): void {
     // fatals are always logged unless level = LOG_COMPLETE_SILENCE
     if (this._level <= CNLogger.CNLogLevel.LOG_QUIET) {
-      args[0] = `FATAL: ${this._name}: ${args[0]}`;
-      console.error(...args);
+      let msg = util.format(
+        `FATAL: ${this._name}: ${args[0]}`,
+        ...args.slice(1),
+      );
+      console.error(msg);
     }
   }
 
   error(...args: any): void {
     // errors are always logged unless level = LOG_COMPLETE_SILENCE
     if (this._level <= CNLogger.CNLogLevel.LOG_QUIET) {
-      args[0] = `ERROR: ${this._name}: ${args[0]}`;
-      console.error(...args);
+      let msg = util.format(
+        `ERROR: ${this._name}: ${args[0]}`,
+        ...args.slice(1),
+      );
+      console.error(msg);
     }
   }
 
   warn(...args: any): void {
     // warnings are always logged unless level = LOG_COMPLETE_SILENCE
     if (this._level <= CNLogger.CNLogLevel.LOG_QUIET) {
-      args[0] = `WARN: ${this._name}: ${args[0]}`;
-      console.warn(...args);
+      let msg = util.format(
+        `WARN: ${this._name}: ${args[0]}`,
+        ...args.slice(1),
+      );
+      console.warn(msg);
     }
   }
 
   info(...args: any): void {
     if (this._level <= CNLogger.CNLogLevel.LOG_INFO) {
-      args[0] = `INFO: ${this._name}: ${args[0]}`;
-      console.info(...args);
+      let msg = util.format(
+        `INFO: ${this._name}: ${args[0]}`,
+        ...args.slice(1),
+      );
+      //args[0] = `INFO: ${this._name}: ${args[0]}`;
+      console.info(msg);
     }
   }
 
   debug(...args: any): void {
     if (this._level <= CNLogger.CNLogLevel.LOG_DEBUG) {
-      args[0] = `DEBUG: ${this._name}: ${args[0]}`;
-      console.info(...args);
+      let msg = util.format(
+        `DEBUG: ${this._name}: ${args[0]}`,
+        ...args.slice(1),
+      );
+      console.info(msg);
     }
   }
 
   trace(...args: any): void {
     if (this._level <= CNLogger.CNLogLevel.LOG_TRACE) {
-      args[0] = `TRACE: ${this._name}: ${args[0]}`;
-      console.info(...args);
+      let msg = util.format(
+        `TRACE: ${this._name}: ${args[0]}`,
+        ...args.slice(1),
+      );
+      console.info(msg);
     }
   }
 
   force(...args: any): void {
     // forces are always logged even if level = LOG_COMPLETE_SILENCE
-    args[0] = `FORCED: ${this._name}: ${args[0]}`;
-    console.error(...args);
+    let msg = util.format(
+      `FORCED: ${this._name}: ${args[0]}`,
+      ...args.slice(1),
+    );
+    console.error(msg);
   }
 }
 
